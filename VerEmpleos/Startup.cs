05b8ae8bdef086a52b152e1aa6a9748ebd3bf0f7
Andrés Guerrero <aguerrerom0711@gmail.com>
2016-07-08 01:32:12 -0500

    First Commit

---
+++ VerEmpleos/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VerEmpleos.Startup))]
+namespace VerEmpleos
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
