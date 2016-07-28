5c40ad2ac9ad0da05360871d1c27a9573266c164
Andrés Guerrero <aguerrerom0711@gmail.com>
2016-07-27 23:48:15 -0500

    Country Service Add

---
+++ BusinessLayer/Services/CountryService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using BusinessLayer.Models;
+using DataLayer.Repository.VacanteRepository;
+
+namespace BusinessLayer.Services
+{
+    static public class CountryService
+    {
+         public static IEnumerable<CountryModel> GetAllCountries()
+        {
+            try
+            {
+                using (var repository = new CountryRespository())
+                {
+                    return repository.DbConn.pais
+                        .Select(e => new CountryModel
+                        {
+                            id = e.idPais,
+                            name = e.Pais1
+                        }).ToList();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+}
