9f73b9947985f2ef0b816475a4d9c252c6fd023f
Elizabeth Guerrero <elizabethgm.81416@gmail.com>
2016-07-28 01:41:43 -0500

    GetCity Add

---
+++ BusinessLayer/Services/CityService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using BusinessLayer.Models;
+using DataLayer.Repository.VacanteRepository;
+
+namespace BusinessLayer.Services
+{
+    static public class CityService
+    {
+        public static IEnumerable<CityModel> GetCity(int IdEstado)
+        {
+            try
+            {
+                using (var repository = new CityRepository())
+                {
+                    return repository.DbConn.ciudad.Where(e => e.idEstado == IdEstado)
+                        .Select(e => new CityModel
+                        {
+                            IdCity = e.idCiudad,
+                            City = e.Ciudad1,
+                            IdState = e.idEstado,
+                           
+                        }).ToList();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+}
+
---
+++ VerEmpleosWebAPI/Controllers/CityController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BusinessLayer.Services;
+
+namespace VerEmpleosWebAPI.Controllers
+{
+    public class CityController : ApiController
+    {
+
+        public IEnumerable GetCity(int id)
+        {
+            return CityService.GetCity(id);
+        }
+    }
+}
